d0b7c19c6436c3c36945827e09add1979415f023
maxim2525 <maxim.komov25@gmail.com>
2025-08-18 23:10:11 +0200

    Update ios_calendar_icon.sh

--- ios_calendar_icon.sh
+++ ios_calendar_icon.sh
@@ -10,8 +10,8 @@ DAY_NUMBER=$(date +%-d)
 
 convert ios_calendar_blank.png \
   -background none \
-  -gravity North -pointsize 403 -font "~/.local/share/fonts/CamberTRIAL-Rg.otf" \
+  -gravity North -pointsize 403 -font "home/user/.local/share/fonts/CamberTRIAL-Rg.otf" \
   -fill red -annotate +0+170 "$DAY_NAME" \
-  -gravity Center -pointsize 1400 -font "~/.local/share/fonts/Salve-Sans.otf" \
+  -gravity Center -pointsize 1400 -font "/home/user/.local/share/fonts/Salve-Sans.otf" \
   -fill black -annotate +0+350 "$DAY_NUMBER" \
-  ios_calendar_.png
+  ios_calendar.png
