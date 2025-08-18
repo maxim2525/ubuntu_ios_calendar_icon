9226a11542317b07a9de13dc0d9f7e8c9793e2f1
maxim2525 <maxim.komov25@gmail.com>
2025-08-19 00:29:07 +0200

    Update ios_calendar_icon.sh

--- ios_calendar_icon.sh
+++ ios_calendar_icon.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+# Get directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Set the path to your .desktop file
-DESKTOP_FILE="/home/user/.local/share/applications/ios-shortcut.desktop"
+DESKTOP_FILE="$HOME/.local/share/applications/ios_calendar.desktop"
 
-# Get current date parts
+#Get current date info
+
 #uncomment first DAY_NAME and comment second one to show the full day name
-
 #DAY_NAME=$(date +%A) 
+
 DAY_NAME=$(date +%a | tr '[:lower:]' '[:upper:]' | cut -c1-2)
-
 DAY_NUMBER=$(date +%-d)
-MONTH_DAY=$(date +%m%d)  # e.g., 0923
+MONTH_DAY=$(date +%m%d)
 
-# Set icon output path with date-based filename
+# Build icon path relative to script location
 ICON_NAME="ios_calendar_${MONTH_DAY}.png"
-ICON_PATH="$HOME/apps/icons/$ICON_NAME"
+ICON_PATH="${SCRIPT_DIR}/${ICON_NAME}"
 
 # Create the new icon
-convert "$HOME/apps/icons/ios_calendar_blank.png" \
+convert "${SCRIPT_DIR}/ios_calendar_blank.png" \
   -background none \
-  -gravity North -pointsize 403 -font "$HOME/shortcuts/fonts/CamberTRIAL-Rg.otf" \
+  -gravity North -pointsize 403 -font "$HOME/.local/share/fonts/CamberTRIAL-Rg.otf" \
   -fill red -annotate +0+170 "$DAY_NAME" \
-  -gravity Center -pointsize 1400 -font "$HOME/shortcuts/fonts/Salve-Sans.otf" \
+  -gravity Center -pointsize 1400 -font "$HOME/.local/share/fonts/Salve-Sans.otf" \
   -fill black -annotate +0+350 "$DAY_NUMBER" \
   "$ICON_PATH"
 
-# Update the .desktop file's Icon= line
-# Note: we use sed to replace the line starting with Icon=
+# Update the .desktop file to point to the new icon
 sed -i "s|^Icon=.*|Icon=$ICON_PATH|" "$DESKTOP_FILE"
